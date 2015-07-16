a4df1645082e9b3a4e2f11d52e646e4faae8230f
Bruno <baclpt@gmail.com>
2015-07-17 00:02:09 +0100

    Add extra jokes

--- jokesproviderjavalib/src/main/java/com/baclpt/joker/JokesProvider.java
+++ jokesproviderjavalib/src/main/java/com/baclpt/joker/JokesProvider.java
@@ -7,9 +7,27 @@ package com.baclpt.joker;
 public class JokesProvider {
     /**
      * Return a random joke
+     *
      * @return string with a random joke
      */
     public String getJoke() {
-        return "funny joke";
+        // Just as proof of concept, added extra jokes
+        // source: http://www.jokes4us.com/
+
+        int index = (int) (Math.random() * 3);
+        switch (index) {
+            case 0:
+                return "Q: Why was 6 afraid of 7?\n" +
+                        "A: Because 7, 8, 9.";
+            case 1:
+                return "Q: What did the apple say to the apple pie?\n" +
+                        "A: You've got some crust.";
+            default:
+                return "Q: Where do birds go for coffee?\n" +
+                        "A: on a NESTcafe.";
+
+        }
+
     }
 }
+
