5015d619ad6611be86023187c44686709c99c359
Bruno <baclpt@gmail.com>
2015-07-17 16:08:47 +0100

    Update JokeDisplayLibrary  no joke found case

--- jokesdisplayer/src/main/java/com/baclpt/jokesdisplayer/JokeTellerActivity.java
+++ jokesdisplayer/src/main/java/com/baclpt/jokesdisplayer/JokeTellerActivity.java
@@ -1,10 +1,9 @@
 package com.baclpt.jokesdisplayer;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 
@@ -16,34 +15,18 @@ public class JokeTellerActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_teller);
+        setTitle(R.string.title_random_joke);
+
         final Intent intent = getIntent();
-        if (intent != null) {
-            final String joke = intent.getStringExtra(JOKE_TEXT_EXTRA);
-            final TextView tvJoke = (TextView) findViewById(R.id.joke_text_view);
-            tvJoke.setText(joke);
-            //TODO: null case
-        }
-    }
+        final String joke;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_joke_teller, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (intent != null && intent.getStringExtra(JOKE_TEXT_EXTRA) != null) {
+            joke = intent.getStringExtra(JOKE_TEXT_EXTRA);
+        } else {
+            joke = getString(R.string.error_msg_joke_not_found);
         }
 
-        return super.onOptionsItemSelected(item);
+        final TextView tvJoke = (TextView) findViewById(R.id.joke_text_view);
+        tvJoke.setText(joke);
     }
 }
