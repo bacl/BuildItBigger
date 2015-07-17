3b37b2676789bde41b550f7b78e4e1547b572e0b
Bruno <baclpt@gmail.com>
2015-07-17 16:31:30 +0100

    Add Loading Indicator

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,22 +4,27 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements EndpointsAsyncTask.TaskCallback {
     private InterstitialAd mInterstitialAd;
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (ProgressBar) findViewById(R.id.progressBar);
+        spinner.setVisibility(View.INVISIBLE);
+
 
         // Adding interstitial ads to an activity
         mInterstitialAd = new InterstitialAd(this);
@@ -47,6 +52,10 @@ public class MainActivity extends ActionBarActivity {
 
 
     public void tellJoke(View view) {
+        // show spinner
+        spinner.setVisibility(View.VISIBLE);
+
+        // load interstitial ad if available
         if (mInterstitialAd.isLoaded()) {
             mInterstitialAd.show();
         } else {
@@ -55,7 +64,17 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJokeNow() {
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, EndpointsAsyncTask.WEB_SERVICE_URL));
+        // start async task to get joke from app engine server
+        new EndpointsAsyncTask(this).execute(new Pair<Context, String>(this, EndpointsAsyncTask.WEB_SERVICE_URL));
     }
 
+    @Override
+    public void onDone(String result) {
+        // hide spinner
+        spinner.setVisibility(View.INVISIBLE);
+
+        // check if it was successful on retrieving a joke, if not show error msg
+        if (result == null)
+            Toast.makeText(this, R.string.error_msg_connection, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,22 +7,39 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements EndpointsAsyncTask.TaskCallback {
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        spinner = (ProgressBar) findViewById(R.id.progressBar);
+        spinner.setVisibility(View.INVISIBLE);
+
+    }
 
     public void tellJoke(View view) {
+        // show spinner
+        spinner.setVisibility(View.VISIBLE);
 
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, EndpointsAsyncTask.WEB_SERVICE_URL));
+        // start async task to get joke from app engine server
+        new EndpointsAsyncTask(this).execute(new Pair<Context, String>(this, EndpointsAsyncTask.WEB_SERVICE_URL));
 
     }
 
+    @Override
+    public void onDone(String result) {
+        // hide spinner
+        spinner.setVisibility(View.INVISIBLE);
 
+        // check if it was successful on retrieving a joke, if not show error msg
+        if (result == null)
+            Toast.makeText(this, R.string.error_msg_connection, Toast.LENGTH_SHORT).show();
+    }
 }
