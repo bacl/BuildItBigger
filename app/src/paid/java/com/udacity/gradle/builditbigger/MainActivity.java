4293275801ffe20f17e9664608ff60e6e8c05ecc
Bruno <baclpt@gmail.com>
2015-07-17 17:02:24 +0100

    Manifest Update & Code clean up
    Set JokeTellerActivity parent affinity

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
